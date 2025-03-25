9165d65decea9f9fe8e5e7b91ee5def4fcc1ac0c
nathanielpierson <nathanielepierson@gmail.com>
2025-03-24 20:46:30 -0400

    add some letters

--- src/WordlePage.jsx
+++ src/WordlePage.jsx
@@ -1,3 +1,17 @@
 export function WordlePage() {
-  return <p>"here be wordle"</p>;
+  const lowercaseAsciiStart = 97;
+  function pickRandom() {
+    const letterIndex = Math.floor(Math.random() * 26);
+    const letter = String.fromCharCode(lowercaseAsciiStart + letterIndex);
+  }
+  var one = letter;
+  var two = "e";
+  var three = "l";
+  var four = "l";
+  var five = "o";
+  return (
+    <div>
+      <p>{one + two + three + four + five}</p>
+    </div>
+  );
 }
